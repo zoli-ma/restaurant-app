953d00a4c15f87205f5e12cb5a89a6486e3d5333
zoli-ma <zoli121234@gmail.com>
2026-07-01 13:21:56 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,21 @@
-self.addEventListener("install", e => {
-    self.skipWaiting();
+const CACHE = "restaurant-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", e => {});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
